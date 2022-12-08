b482bd9b79135f4beec1c38c82ee8e7aec69392b
sdunchev <stoyandunchev@gmail.com>
2022-12-08 07:58:48 +0000

    Test the development branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my project ! Edit <code>src/App.js</code> and save to reload.
+          My name is Stoyan! Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
