8467fe53f1776a4fa6acf1f1eb51499b67e7a5e3
sdunchev <stoyandunchev@gmail.com>
2022-12-08 10:26:07 +0000

    Testing the develpment branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My name is Stoyan Dunchev! 
+          Hello, my name is Stoyan Dunchev! 
         </p>
         <a
           className="App-link"
